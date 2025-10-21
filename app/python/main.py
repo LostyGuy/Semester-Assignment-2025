07f1d141403afb060636f90688223bf2d862b9e9
LostyGuy <89598637+LostyGuy@users.noreply.github.com>
2025-10-21 11:59:08 +0200

    Created templates for DJango library, created simple user creation logic, added logging system,

--- app/python/main.py
+++ app/python/main.py
@@ -1,27 +1,92 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Depends
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
+from python import models, database
+from sqlalchemy.orm import Session
+from pathlib import Path
+import logging as log
+import hashlib
+import time as t
+
+log.basicConfig(filemode="a", 
+                filename= str(Path(__file__).resolve().parent.parent / "apps_log.log"),
+                level=log.INFO,
+                )
+
+def log_info(*message) -> None:
+    log.info("-" * 20)
+    log.info(message)
+
+def current_time() -> str:
+    ts = t.strftime("%Y-%m-%d %H:%M:%S", t.localtime())
+    return ts
 
 app = FastAPI()
-templates = Jinja2Templates(directory=r"./templates/html")
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+app.mount("/static", StaticFiles(directory=str(BASE_DIR / "templates")), name="static")
+templates = Jinja2Templates(directory=str(BASE_DIR / "templates/html"))
 
 @app.get("/", response_class=HTMLResponse)
 async def main_page(request: Request):
+    a = 1
+    return templates.TemplateResponse("index.html", {"request" : request, "a" : a})
 
-    #raise Exception(NotImplementedError)
+@app.get("/login", response_class=HTMLResponse)
+async def main_page(request: Request):
 
     return templates.TemplateResponse("index.html", {"request" : request})
 
-@app.get("profile", response_class=HTMLResponse)
-async def profile(request: Request):
+@app.get("/register", response_class=HTMLResponse)
+async def register_page(request: Request):
 
-    #raise Exception(NotImplementedError)
+    return templates.TemplateResponse("register.html", {"request" : request})
 
+@app.post("/register_request", response_class=HTMLResponse)
+async def register_request(request: Request, db: Session = Depends(database.get_db)):
+    salt = "ff" # later on stored in DB
+    
+    form_data = await request.form()
+    
+    form_nickname: str = form_data.get("nickname")
+    form_email: str = form_data.get("email")
+    form_passwd: str = form_data.get("passwd")
+    
+    hashed_passwd = hashlib.sha256((form_passwd + salt).encode()).hexdigest()
+    
+    register_new_user = models.users(
+        nickname = form_nickname,
+        email = form_email,
+        hashed_password = hashed_passwd,
+        time_stamp = current_time()
+    )
+    
+    try:
+        db.add(register_new_user)
+        db.commit()
+    except Exception as e:
+        db.rollback()
+        log_info(e)
+        register_status: bool = False
+    finally:
+        new_user: int = db.query(models.users.ID).filter(models.users.email == register_new_user.email)
+        register_status = True if new_user else False
+            
+
+    return templates.TemplateResponse("register.html", {"request" : request, "register_status" : register_status})
+
+@app.get("/profile", response_class=HTMLResponse)
+async def profile(request: Request, db: Session = Depends(database.get_db)):
+    logged_user_ID = ...
+    
+    get_projects = db.query(models.profile).filter(
+        models.profile.user_ID == logged_user_ID)
+    
     return templates.TemplateResponse("profile.html", {"request" : request})
 
-@app.get("search", response_class=HTMLResponse)
+@app.get("/search", response_class=HTMLResponse)
 async def search(request: Request):
-
-    #raise Exception(NotImplementedError)
 
     return templates.TemplateResponse("search.html", {"request" : request})
--- app/python/models.py
+++ app/python/models.py
@@ -1,4 +1,47 @@
 from sqlalchemy import Column, Integer, String, Float, TIMESTAMP
 from python.database import Base
 
-# class name(Base):
+class users(Base):
+    __tablename__ = "users"
+    ID: int = Column(
+        Integer,
+        primary_key=True,
+        autoincrement=True,
+        index=True
+        )
+    nickname: str = Column(
+        String(20),
+        index=True
+        )
+    email: str = Column(
+        String(20),
+        index=True
+        )
+    hashed_password: str = Column(
+        String(30),
+        index=True
+        )
+    time_stamp: str = Column(
+        String(10),
+        index=True
+        )
+    
+class profile(Base):
+    __tablename__ = "profiles"
+    ID: int = Column(
+        Integer,
+        primary_key=True,
+        autoincrement=True,
+        index=True
+        )
+    user_ID : int = Column(
+        Integer,
+        index=True)
+    content: str = Column(
+        String(20),
+        index=True
+        )
+    time_stamp: str = Column(
+        String(10),
+        index=True
+        )
