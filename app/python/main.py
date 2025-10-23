79057002f5c41184790a94218b9bf94a9ab52cb7
LostyGuy <89598637+LostyGuy@users.noreply.github.com>
2025-10-23 08:46:53 +0200

    Creating login page

--- app/python/main.py
+++ app/python/main.py
@@ -22,6 +22,8 @@ def current_time() -> str:
     ts = t.strftime("%Y-%m-%d %H:%M:%S", t.localtime())
     return ts
 
+hash_salt = "ff"
+
 app = FastAPI()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -35,9 +37,28 @@ async def main_page(request: Request):
     return templates.TemplateResponse("index.html", {"request" : request, "a" : a})
 
 @app.get("/login", response_class=HTMLResponse)
-async def main_page(request: Request):
+async def login_page(request: Request):
+    
+    return templates.TemplateResponse("login.html", {"request" : request})
 
-    return templates.TemplateResponse("index.html", {"request" : request})
+@app.post("/login_request", response_class=HTMLResponse)
+async def login_request(hash_salt, request: Request, db: Session = Depends(database.get_db)):
+    
+    login_data = await request.form()
+    
+    def get_data(login_data):
+        login: str = login_data.get("email")
+        hashed_password: str = hashlib.sha256((login_data.get("passwd") + hash_salt).encode()).hexdigest()
+        return login, hashed_password
+    
+    def login_attempt(login, hashed_password):
+        login_attempt = db.query(models.users.ID).filter(models.users.email == login and models.users.hashed_password == hashed_password)
+        
+        
+    login, hashed_password = get_data(login_data=login_data)
+    login_attempt(login = login, hashed_password= hashed_password)
+    
+    return templates.TemplateResponse("login.html", {"request" : request})
 
 @app.get("/register", response_class=HTMLResponse)
 async def register_page(request: Request):
@@ -45,8 +66,7 @@ async def register_page(request: Request):
     return templates.TemplateResponse("register.html", {"request" : request})
 
 @app.post("/register_request", response_class=HTMLResponse)
-async def register_request(request: Request, db: Session = Depends(database.get_db)):
-    salt = "ff" # later on stored in DB
+async def register_request(hash_salt, request: Request, db: Session = Depends(database.get_db)):
     
     form_data = await request.form()
     
@@ -54,7 +74,7 @@ async def register_request(request: Request, db: Session = Depends(database.get_
     form_email: str = form_data.get("email")
     form_passwd: str = form_data.get("passwd")
     
-    hashed_passwd = hashlib.sha256((form_passwd + salt).encode()).hexdigest()
+    hashed_passwd = hashlib.sha256((form_passwd + hash_salt).encode()).hexdigest()
     
     register_new_user = models.users(
         nickname = form_nickname,
@@ -62,7 +82,7 @@ async def register_request(request: Request, db: Session = Depends(database.get_
         hashed_password = hashed_passwd,
         time_stamp = current_time()
     )
-    
+
     try:
         db.add(register_new_user)
         db.commit()
@@ -83,6 +103,11 @@ async def profile(request: Request, db: Session = Depends(database.get_db)):
     
     get_projects = db.query(models.profile).filter(
         models.profile.user_ID == logged_user_ID)
+    
+    return templates.TemplateResponse("profile.html", {"request" : request})
+
+@app.get("/profile/add_project", response_class=HTMLResponse)
+async def profile(request: Request, db: Session = Depends(database.get_db)):
     
     return templates.TemplateResponse("profile.html", {"request" : request})
 
